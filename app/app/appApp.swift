16527aaef9300c66e94f25628c283e455a9210ba
Evgeniy Pavlov <evg-pavlov@172.24.31.239-spb.dhcp.yndx.net>
2025-11-05 19:46:35 +0300

    init

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Evgeniy Pavlov on 05.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
