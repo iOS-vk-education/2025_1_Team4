01056ae9cb59a97f48acdc1f3bd006109c502af0
Evgeniy Pavlov <evg-pavlov@172.24.31.239-spb.dhcp.yndx.net>
2025-11-06 15:30:19 +0300

    resolve issues

---
+++ NoteHub/NoteHub/NoteHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteHubApp.swift
+//  NoteHub
+//
+//  Created by Evgeniy Pavlov on 06.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
