09f039b0a1dd175273e9e6327ef08b4e3a543ed2
Evgeniy Pavlov <evg-pavlov@172.31.21.247-vpn.dhcp.yndx.net>
2025-12-07 14:47:50 +0300

    add user profile

--- NoteHub/NoteHub/Models/AuthDataResultModel.swift
+++ NoteHub/NoteHub/Models/AuthDataResultModel.swift
@@ -11,11 +11,9 @@ import FirebaseAuth
 struct AuthDataResultModel {
     let uid: String
     let email: String
-    let name: String
     
     init(user: User) {
         self.uid = user.uid
         self.email = user.email!
-        self.name = user.displayName ?? "Anonymous"
     }
 }
